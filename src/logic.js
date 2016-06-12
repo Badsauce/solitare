c077d2081c92463d854e2597d2d04bf974a4fc88
badsauce <nickrutowski@gmail.com>
2016-06-11 20:45:16 -0700

    added axios for ajax calls and http://deckofcardsapi.com/ for deck and shuffling

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,16 @@
-ReactDOM.render(
-  <h1>I'm going to build a solitare app here</h1>,
-  document.getElementById('example')
-);
+var deck_id
+
+axios.get('http://deckofcardsapi.com/api/deck/new/')
+  .then(function (response) {
+    deck_id = response.data.deck_id;
+
+    ReactDOM.render(
+      <h1>Deck id is {deck_id}</h1>,
+      document.getElementById('example')
+    );
+  })
+  .catch(function (response) {
+    console.log('Error when submitting new deck request.');
+  });
+
+console.log(deck_id);
