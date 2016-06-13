3b10058ab9c157f9e556586b8f3de6ab99e78e66
badsauce <nickrutowski@gmail.com>
2016-06-12 21:33:21 -0700

    began work on a deal function that wraps the cardapi draw endpoint

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,5 @@
 var deck_id
+var tableau = [[],[],[],[],[],[],[]]
 
 axios.get('http://deckofcardsapi.com/api/deck/new/')
   .then(function (response) {
@@ -8,9 +9,29 @@ axios.get('http://deckofcardsapi.com/api/deck/new/')
       <h1>Deck id is {deck_id}</h1>,
       document.getElementById('example')
     );
+
+    console.log(deck_id);
+    for(var i = 0;i < 7; i++){
+      deal(i+1,tableau[i]);
+    }
+    setTimeout(function () {
+      console.log(tableau);
+    }, 1000);
   })
   .catch(function (response) {
     console.log('Error when submitting new deck request.');
+    ReactDOM.render(
+      <h1>Error contacting cards API</h1>,
+      document.getElementById('example')
+    );
   });
 
-console.log(deck_id);
+function deal(count,pile){
+  axios.get('http://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count='+count)
+   .then(function(response){
+     pile[0] = response;
+   })
+   .catch(function(response){
+     console.log('Error when submitting drawing a new card.');
+   })
+}
